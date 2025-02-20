6352ee09b6cb4109e363cd54114e3475ec947920
Syarif <sulthansyarif784@gmail.com>
2025-02-20 08:54:45 +0700

    final part 3

--- resources/views/components/feature-card.blade.php
+++ resources/views/components/feature-card.blade.php
@@ -2,8 +2,8 @@
 
 <div class="bg-white rounded-lg shadow-lg p-6 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-xl">
 
-    <div class="flex justify-center">
-        <i class="fa-solid {{ $image }} {{ $color }} w-32 h-32 mx-auto my-8"></i>
+    <div class="flex justify-center p-8">
+        <i class="fa-solid {{ $image }} {{ $color }} w-20 h-20 mx-auto my-8"></i>
     </div>
     <h3 class="text-xl font-semibold text-gray-900 text-center mb-2">{{ $title }}</h3>
     <p class="text-gray-600 text-center">{{ $description }}</p>
